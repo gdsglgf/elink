3723f6d25432a2ccdf6effc35fd32f8ba823b134
gdsglgf <wzguofulgz@126.com>
2016-12-09 16:25:15 +0800

    init miner

---
+++ miner/src/main/java/com/miner/Main.java
@@ -0,0 +1,9 @@
+package com.miner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+	}
+
+}
---
+++ miner/src/test/java/com/miner/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.miner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+	
+	@Test
+	public void test() {
+		Assert.assertEquals(true, 5 > 3);
+	}
+}
